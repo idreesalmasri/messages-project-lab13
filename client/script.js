5c014e04c579e58db122a7d0ae4315ab3b3dd86b
idreesalmasri <idrees.massri95@gmail.com>
2022-03-27 02:54:12 +0300

    done

--- client/script.js
+++ client/script.js
@@ -1,25 +1,46 @@
-// "use strict"
-// const client = require('socket.io-client');
-// const host = "http://localhost:3000";
-// const socket = client.connect(host);
-// socket.on("connect",()=>{
-//     console.log("connected",socket.id);
-// })
-// socket.emit('get_all');
-// // socket.emit('send-message', "message")
-// socket.on("receive-message", message => {
-//     let payload=message.payload;
-//     console.log(payload);
-//     socket.emit('received', message);
-    
-// })
-
-// const value3 = process.argv.splice(2); //node parent.js 'eat your food' 'go to school'
-// // value3 >>> ['eat your food', 'go to school']
-// value3.forEach((task) => {
-//     socket.emit('send-message', task)
-// })
-// socket.on('added', () => {
-    
-//     socket.disconnect();// this is not a mandatory
-// })
+'use strict';
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3000");
+const joinRoomButton = document.getElementById('room-button')
+const messageInput = document.getElementById('message-input')
+const roomInput = document.getElementById('room-input')
+const form = document.getElementById('form')
+const userSocket = io('http://localhost:3000/user', { auth: { token: 'please send token' } })
+userSocket.on('connect_error', error => {
+    displayMessage(error)
+})
+socket.on('connect', () => {
+    displayMessage(`You connected with id ${socket.id}`)
+})
+socket.on('receive-message', message => {
+    displayMessage(message)
+})
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    const room = roomInput.value
+    if (message === '') return
+    displayMessage(message)
+    socket.emit('send-message', message, room)
+    messageInput.value = ''
+})
+joinRoomButton.addEventListener('click', () => {
+    const room = roomInput.value
+    socket.emit('join-room', room, message => {
+        displayMessage(message)
+    })
+})
+function displayMessage(message) {
+    const div = document.createElement('div')
+    div.textContent = message
+    document.getElementById('message-container').append(div)
+}
+let count = 0;
+setInterval(() => {
+    socket.emit('ping', ++count)
+}, 1000)
+document.addEventListener('keydown', e => {
+    if (e.target.matches('input')) return
+    if (e.key === 'c') socket.connect()
+    if (e.key === 'd') socket.disconnect()
+})
